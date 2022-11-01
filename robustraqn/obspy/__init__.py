4bb0e42612fa8b7007c8b519a39c61a7baca91c5
flixha <felix.halpaap@uib.no>
2022-11-01 16:13:28 +0100

    add init files to submodules

---
+++ robustraqn/obspy/__init__.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python
+"""
+:copyright:
+    RobustRAQN developers.
+
+:license:
+    GNU General Public License, Version 3
+    (https://www.gnu.org/licenses/gpl-3.0)
+"""
+import importlib
+import sys
+import warnings
+
+
+# from robustraqn import quality_metrics

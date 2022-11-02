01d85b79846c803f0bd043c2cdac2f871f189b8f
flixha <felix.halpaap@uib.no>
2022-11-02 09:21:48 +0100

    properly add sds client mod

---
+++ robustraqn/obspy/clients/__init__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+# -----------------------------------------------------------------------------
+#  Purpose: Extend Core classes of ObsPy: Python for Seismological Observatories
+#   Author: Felix Halpaap
+# -----------------------------------------------------------------------------
+"""
+robustraqn.obspy.core - Extended Core classes of ObsPy
+==================================
+"""
+
+from robustraqn.obspy.core.trace import Trace  # NOQA
+from robustraqn.obspy.core.stream import Stream  # NOQA
+
+
+#if __name__ == '__main__':
+#    import doctest
+#    doctest.testmod(exclude_empty=True)

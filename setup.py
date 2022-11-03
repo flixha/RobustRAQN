f2c3fa8d1c00e76ed9212f9c57f27229e45e502c
flixha <felix.halpaap@uib.no>
2022-11-03 17:24:44 +0100

    add model file as data_files

--- setup.py
+++ setup.py
@@ -21,8 +21,9 @@ setup(
     # packages=['robustraqn', 'robustraqn.models', 'robustraqn.obspy'],
     packages=find_packages(),
     # packages=find_packages(exclude=['ez_setup', 'tests', 'tests.*']),
-    package_data={'': ['license.txt',
-                       'robustraqn/models/NNSN1D_plusAK135.tvel' ]},
+    package_data={'': ['license.txt']},
+    data_files=[
+        ('velocity_models', ['robustraqn/models/NNSN1D_plusAK135.tvel'])],
     # include_package_data=True,
     long_description=read('README.md'),
     classifiers=[
